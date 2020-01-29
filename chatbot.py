d385bc66ec8023e8642264bc3ba407d9d61088f0
SOBIN SUNNY <44584582+sobin-sunny@users.noreply.github.com>
2020-01-30 02:26:56 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+#Importing Libraries
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+
+#Defining bot profile
+bot=ChatBot("CHOTTU")
+bot.set_trainer(ChatterBotCorpusTrainer)
+
+#Training the chatbot with corpus
+bot.train("chatterbot.corpus.english")
+
+#Getting input and giving response
+while(1):
+    message=input("You: ")
+    if(message!="Bye" or message!="bye"):
+        print("{}: {}".format(bot.name,bot.get_response(message)))
+    if(message=="Bye" or message=="bye"):
+              print("{}: I will really miss you. Bye".format(bot.name))
+              break
